bb5b181fd7ae6b50042d01395d493328a6067eea
cuijiahuan <1290829466@qq.com>
2018-01-25 03:34:01 +0800

    崔佳欢提交代码，明天添加ajax

---
+++ src/components/slide/slide.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="banner">
+    <slide :slides="slides" :inv="inv" :style="styleObject"></slide>
+  </div>
+</template>
+
+<script>
+import slide from '@/components/banner/banner.vue'
+export default {
+  components: {slide},
+  data () {
+    return {
+      slides: [
+        {
+          src: 'http://i3.mifile.cn/a4/xmad_14913974588712_tsRvc.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  #banner{
+    margin-top:80px;
+    #image{
+      width:100%;
+      height:300px;
+      position:relative;
+      overflow:hidden;
+      #image li{
+        float:left;
+        width:100%;
+        height:300px;
+      }
+    }
+  }
+</style>
