baed5f6461b5f6b44b325d3553aa9ee5b0bf57cd
cuijiahuan <1290829466@qq.com>
2018-01-27 01:50:51 +0800

    崔佳欢提交代码，明天编写购物车

---
+++ src/components/shcontent/shcontent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="shoppingcontent">
+
+  </div>
+</template>
---
+++ src/components/shtop/shtop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="shoppingtop">
+    <span id="sp1">购物车</span>
+    <span id="sp2"><a href="#">编辑</a></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #shoppingtop{
+    width:100%;
+    height:100px;
+    background-color:#ff0000;
+    span{
+      line-height:100px;
+      color:#fff;
+    }
+    a{
+      color:#fff;
+    }
+    #sp1{
+      font-size:30px;
+      margin-left:300px;
+    }
+    #sp2{
+      font-size:25px;
+      margin-left:200px;
+    }
+  }
+</style>
